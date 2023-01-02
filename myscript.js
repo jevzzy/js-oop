8a872270f1867e66bbbbbeb9a37ebc6a49aee896
judewrld <jevzzy007@gmail.com>
2023-01-02 20:10:42 +0100

    aha a

--- myscript.js
+++ myscript.js
@@ -1,24 +1,3 @@
-function displayarray(){
-    for(let i = 0; i < myLibrary.length; i++) {
-//loops create div element according to how many object in library array
-    let cards = document.createElement('div')
-    cards.classList.add('cards')
-    let title = document.createElement('h5')
-    title.innerText = `title: ${myLibrary[i].title}`
-    let author = document.createElement('h5')
-    author.innerText = `author: ${myLibrary[i].author}`
-    let pages = document.createElement('h5')
-    pages.innerText = `pages: ${myLibrary[i].pages}`
-    cards.appendChild(title)
-    cards.appendChild(author)
-    cards.appendChild(pages)
-    let mybooks = document.querySelector('.mylibrary')
-    mybooks.append(cards)
-    
-}
-}
-
-
 
 //empty library
 let myLibrary = []
@@ -44,27 +23,65 @@ myLibrary.push(titan)
 
 
 
-
-
-// function to add book to array
+// function tto add book to array
 function addBookToLibrary(){
     let titleInput = document.getElementById('title')
-let authorInput = document.getElementById('author')
-let pageInput = document.getElementById('page')
-let author = authorInput.innerText
-let page = pageInput.innerText
-let title = titleInput.innerText
-const newBook = new Book(title,page,author)
- 
- return myLibrary.push(newBook)
+    let authorInput = document.getElementById('author')
+    let pageInput = document.getElementById('page')
+    let author = authorInput.innerText
+    let page = pageInput.innerText
+    let title = titleInput.innerText
+let newBook = new Book(title,page,author)
+myLibrary.push(newBook)
+console.log(myLibrary)
+displayarray()
 }
-console.log(addBookToLibrary())
+
+
+document.addEventListener("DOMContentLoaded", (e)=>{
+
+    displayarray()
+})
 
 const addBtn = document.querySelector('.add')
-addBtn.addEventListener("click", addBookToLibrary)
 
-console.log(myLibrary)
-console.log(displayarray())
+addBtn.addEventListener("mousedown", (e)=>{
+console.log(e)
+addBookToLibrary()
+
+
+})
+
+document.querySelector(".book-form").addEventListener("submit", (e)=>{
+    e.preventDefault();
+  });
+  
+
+
+let mybooks = document.querySelector('.mylibrary')
+function displayarray(){
+   
+    for(let i = 0; i < myLibrary.length; i++) {
+     
+//loops create div element according to how many object in library array
+    let cards = document.createElement('div')
+    cards.classList.add('cards')
+    let title = document.createElement('h5')
+    title.innerText = `title: ${myLibrary[i].title}`
+    let author = document.createElement('h5')
+    author.innerText = `author: ${myLibrary[i].author}`
+    let pages = document.createElement('h5')
+    pages.innerText = `pages: ${myLibrary[i].pages}`
+    cards.appendChild(title)
+    cards.appendChild(author)
+    cards.appendChild(pages)
+
+    mybooks.appendChild(cards)
+
+}
+
+}
+
 
 
 
@@ -107,7 +124,6 @@ console.log(displayarray())
 const bookBtn = document.querySelector('.new') 
 const newbook = document.querySelector('.book-form');
 const cancelBtn = document.querySelector('.cancel')
-
 
 
 
